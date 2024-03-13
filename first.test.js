eb67030a462bddb549aac2e0dee27ecebe255a74
Monika-1709 <mrana@griddynamics.com>
2024-03-13 23:58:08 +0530

    unit testing

---
+++ first.test.js
@@ -0,0 +1,21 @@
+describe("shoild ", () => {
+  test("should ", () => {
+    expect(1 + 2).toBe(3);
+    expect(1 + 2).toBe(3);
+    expect(1 + 2).toBe(3);
+  });
+
+  it("should 2", () => {
+    expect(3 + 2).toBe(5);
+  }); //it is work similar to test
+}); //describe is used to combined multiple teste in one
+
+describe("shoild ", () => {
+  // test("should ", () => {
+  //   expect(1 + 2).toBe(3);
+  // });
+
+  it("should 3", () => {
+    expect(3 + 9).toBe(12);
+  }); //it is work similar to test
+});
